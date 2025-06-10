d2f2bd848aef38ab033068fcdfa457a441421099
Shubham saurabh <shubhamsaurabhk@gmail.com>
2025-06-10 22:51:48 +0530

    link fixes

--- src/App.vue
+++ src/App.vue
@@ -132,7 +132,15 @@ const closeModal = () => {
     <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2v6m-6-6h6a2 2 0 012 2v6a2 2 0 01-2 2H8a2 2 0 01-2-2v-6a2 2 0 012-2z" />
     </svg>
-    <a :href="link" target="_blank" rel="noopener noreferrer" class="hover:underline truncate">{{ link }}</a>
+    <a
+  :href="link.startsWith('http') ? link : 'https://' + link"
+  target="_blank"
+  rel="noopener noreferrer"
+  class="hover:underline truncate"
+>
+  {{ link }}
+</a>
+
   </div>
 </div>
 <!-- Images Section -->
